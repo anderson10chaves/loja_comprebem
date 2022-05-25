b3edcc9bbc3880fc996fdadfaa74f9c398c9ce9b
achaves <achaves@192.168.0.176>
2022-05-25 12:06:19 -0300

    Primeiro Commit

---
+++ loja_comprebem/src/main/java/adcsistemas/loja_comprebem/LojaComprebemApplication.java
@@ -0,0 +1,13 @@
+package adcsistemas.loja_comprebem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaComprebemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaComprebemApplication.class, args);
+	}
+
+}
---
+++ loja_comprebem/src/test/java/adcsistemas/loja_comprebem/LojaComprebemApplicationTests.java
@@ -0,0 +1,13 @@
+package adcsistemas.loja_comprebem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaComprebemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
