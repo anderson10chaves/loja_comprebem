d54b5cac70acc0e660c7803f95e2b3977f19ff22
achaves <achaves@192.168.0.176>
2022-05-26 13:53:31 -0300

    2.4 Spring Data JPA

--- loja_comprebem/src/main/java/adcsistemas/loja_comprebem/model/Endereco.java
+++ loja_comprebem/src/main/java/adcsistemas/loja_comprebem/model/Endereco.java
@@ -5,6 +5,8 @@ import java.util.Objects;
 
 import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +15,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+import adcsistemas.loja_comprebem.enums.TipoEndereco;
 
 @Entity
 @Table(name = "endereco")
@@ -43,6 +47,9 @@ public class Endereco implements Serializable {
 	@JoinColumn(name = "pessoa_id", nullable = false,
 	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
 	private Pessoa pessoa;
+	
+	@Enumerated(EnumType.STRING)
+	private TipoEndereco tipoEndereco;
 
 	public Long getId() {
 		return id;
@@ -114,6 +121,14 @@ public class Endereco implements Serializable {
 
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
+	}
+
+	public TipoEndereco getTipoEndereco() {
+		return tipoEndereco;
+	}
+
+	public void setTipoEndereco(TipoEndereco tipoEndereco) {
+		this.tipoEndereco = tipoEndereco;
 	}
 
 	@Override
