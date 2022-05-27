3a75fcd091eb474fe14f84e75070a66d7cf3e49e
achaves <achaves@192.168.0.176>
2022-05-27 13:29:19 -0300

    2.6 Novas Classes

--- loja_comprebem/src/main/java/adcsistemas/loja_comprebem/LojaComprebemApplication.java
+++ loja_comprebem/src/main/java/adcsistemas/loja_comprebem/LojaComprebemApplication.java
@@ -2,8 +2,10 @@ package adcsistemas.loja_comprebem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "adcsistemas.loja_comprebem.model")
 public class LojaComprebemApplication {
 
 	public static void main(String[] args) {
