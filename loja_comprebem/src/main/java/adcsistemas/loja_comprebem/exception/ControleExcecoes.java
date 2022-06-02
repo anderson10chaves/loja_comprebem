0f1f4f857cc714a177b668d6c97211c8e50e2185
achaves <achaves@192.168.0.176>
2022-06-02 00:10:28 -0300

    4.3 Spring Security e JWT e Excecões

---
+++ loja_comprebem/src/main/java/adcsistemas/loja_comprebem/exception/ControleExcecoes.java
@@ -0,0 +1,84 @@
+package adcsistemas.loja_comprebem.exception;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import adcsistemas.loja_comprebem.model.dto.ObjetoErroDTO;
+
+@RestControllerAdvice
+@ControllerAdvice
+public class ControleExcecoes extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler({ Exception.class, RuntimeException.class, Throwable.class })
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+
+		ObjetoErroDTO objetoErroDTO = new ObjetoErroDTO();
+
+		String msg = "";
+
+		if (ex instanceof MethodArgumentNotValidException) {
+			List<ObjectError> list = ((MethodArgumentNotValidException) ex).getBindingResult().getAllErrors();
+
+			for (ObjectError objectError : list) {
+				msg += objectError.getDefaultMessage() + "\n";
+			}
+		} else {
+			msg = ex.getMessage();
+		}
+
+		objetoErroDTO.setError(msg);
+		objetoErroDTO.setCode(status.value() + " ==> " + status.getReasonPhrase());
+		
+		ex.printStackTrace();
+
+		return new ResponseEntity<Object>(objetoErroDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler({ DataIntegrityViolationException.class, ConstraintViolationException.class, SQLException.class })
+	protected ResponseEntity<Object> handleExceptionDataIntegry(Exception ex) {
+
+		ObjetoErroDTO objetoErroDTO = new ObjetoErroDTO();
+
+		String msg = "";
+
+		if (ex instanceof DataIntegrityViolationException) {
+			msg = "Erro de Integridade do Banco: "
+					+ ((DataIntegrityViolationException) ex).getCause().getCause().getMessage();
+		} else
+
+		if (ex instanceof ConstraintViolationException) {
+			msg = "Erro de chave Estrangeira: "
+					+ ((ConstraintViolationException) ex).getCause().getCause().getMessage();
+		} else
+
+		if (ex instanceof SQLException) {
+			msg = "Erro de SQL do banco de dados: " + ((SQLException) ex).getCause().getCause().getMessage();
+		} else {
+			msg = ex.getMessage();
+		}
+		
+
+		objetoErroDTO.setError(msg);
+		objetoErroDTO.setCode(HttpStatus.INTERNAL_SERVER_ERROR.toString());
+		
+		ex.printStackTrace();
+
+		return new ResponseEntity<Object>(objetoErroDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+
+	}
+}
--- loja_comprebem/src/main/java/adcsistemas/loja_comprebem/security/JWTTokenAutenticacaoService.java
+++ loja_comprebem/src/main/java/adcsistemas/loja_comprebem/security/JWTTokenAutenticacaoService.java
@@ -13,84 +13,87 @@ import org.springframework.stereotype.Service;
 import adcsistemas.loja_comprebem.ApplicationContextLoad;
 import adcsistemas.loja_comprebem.model.Usuario;
 import adcsistemas.loja_comprebem.repository.UsuarioRepository;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
 
 @Service
 @Component
 public class JWTTokenAutenticacaoService {
-	
-	/*Validade Token expira em 11 dias*/
+
+	/* Validade Token expira em 11 dias */
 	private static final long EXPIRATION_TIME = 950400000;
-	
+
 	private static final String SECRET = "@adcsistemas.tecnology";
-	
+
 	private static final String TOKEN_PREFIX = "Bearer";
-	
+
 	private static final String HEADER_STRING = "Authorization";
-	
-	
+
 	public void addAuthentication(HttpServletResponse response, String username) throws Exception {
-		
-		String JWT = Jwts.builder()
-				.setSubject(username)
+
+		String JWT = Jwts.builder().setSubject(username)
 				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
 				.signWith(SignatureAlgorithm.HS512, SECRET).compact();
-		
+
 		String token = TOKEN_PREFIX + " " + JWT;
-		
+
 		response.addHeader(HEADER_STRING, token);
-		
+
 		liberacaoCors(response);
-		
+
 		response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
 	}
-	
+
 	public Authentication getAuthentication(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+
 		String token = request.getHeader(HEADER_STRING);
-		
-		if (token != null) {
-			String tokenLimpo = token.replace(TOKEN_PREFIX, "").trim();
-			
-			String user = Jwts.parser()
-					.setSigningKey(SECRET)
-					.parseClaimsJws(tokenLimpo)
-					.getBody().getSubject();
-			
-			if(user != null) {
-				Usuario usuario = ApplicationContextLoad
-						.getApplicationContext()
-						.getBean(UsuarioRepository.class).findUserByLogin(user);
-				
-				if(usuario != null) {
-					return new UsernamePasswordAuthenticationToken(
-							usuario.getLogin(),
-							usuario.getPassword(),
-							usuario.getAuthorities());
+
+		try {
+
+			if (token != null) {
+				String tokenLimpo = token.replace(TOKEN_PREFIX, "").trim();
+
+				String user = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(tokenLimpo).getBody().getSubject();
+
+				if (user != null) {
+					Usuario usuario = ApplicationContextLoad.getApplicationContext().getBean(UsuarioRepository.class)
+							.findUserByLogin(user);
+
+					if (usuario != null) {
+						return new UsernamePasswordAuthenticationToken(usuario.getLogin(), usuario.getPassword(),
+								usuario.getAuthorities());
+					}
 				}
 			}
-		}
+		} catch (SignatureException e) {
+			response.getWriter().write("Token informado esta inválido ou incorreto");
 		
-		liberacaoCors(response);
+		} catch (ExpiredJwtException e) {
+			response.getWriter().write("Token está expirado, realize o login novamente!");
+
+		} finally {
+			liberacaoCors(response);
+		}
 		return null;
 	}
-	
+
 	private void liberacaoCors(HttpServletResponse response) {
-		
-		if(response.getHeader("Access-Control-Allow-Origin") == null) {
+
+		if (response.getHeader("Access-Control-Allow-Origin") == null) {
 			response.addHeader("Access-Control-Allow-Origin", "*");
 		}
-		
-		if(response.getHeader("Access-Control-Allow-Headers") == null) {
+
+		if (response.getHeader("Access-Control-Allow-Headers") == null) {
 			response.addHeader("Access-Control-Allow-Headers", "*");
 		}
-		
-		if(response.getHeader("Access-Control-Request-Headers") == null) {
+
+		if (response.getHeader("Access-Control-Request-Headers") == null) {
 			response.addHeader("Access-Control-Request-Headers", "*");
 		}
-		
-		if(response.getHeader("Access-Control-Allow-Methods") == null) {
+
+		if (response.getHeader("Access-Control-Allow-Methods") == null) {
 			response.addHeader("Access-Control-Allow-Methods", "*");
 		}
 	}
