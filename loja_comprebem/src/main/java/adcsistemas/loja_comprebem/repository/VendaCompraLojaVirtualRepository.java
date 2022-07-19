a0e8b19e57b2664c836304e66a12a0d19d52843f
achaves <achaves@192.168.0.176>
2022-07-19 03:08:42 -0300

    5.21 Delete com SQl e JDBCTemplate

--- loja_comprebem/src/main/java/adcsistemas/loja_comprebem/repository/VendaCompraLojaVirtualRepository.java
+++ loja_comprebem/src/main/java/adcsistemas/loja_comprebem/repository/VendaCompraLojaVirtualRepository.java
@@ -3,9 +3,6 @@ package adcsistemas.loja_comprebem.repository;
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import adcsistemas.loja_comprebem.model.VendaCompraLojaVirtual;
