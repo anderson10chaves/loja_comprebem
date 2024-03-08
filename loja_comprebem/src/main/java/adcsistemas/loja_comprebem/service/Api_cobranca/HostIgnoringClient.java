d2ffe53173027f8d6930627ce89e44f730117d8f
andersonchaves <andersonchaves@localhost>
2024-03-08 05:23:58 -0300

    8.5 Ignorar SSL e TLS de APIs
    Signed-off-by: andersonchaves <andersonchaves@localhost>

---
+++ loja_comprebem/src/main/java/adcsistemas/loja_comprebem/service/Api_cobranca/HostIgnoringClient.java
@@ -0,0 +1,84 @@
+package adcsistemas.loja_comprebem.service.Api_cobranca;
+
+import java.io.Serializable;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.glassfish.jersey.media.multipart.internal.MultiPartWriter;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.client.urlconnection.HTTPSProperties;
+
+public class HostIgnoringClient implements Serializable {
+	
+	private String hostname;
+	
+	public HostIgnoringClient(String hostname) {
+		this.hostname = hostname;
+	}
+	
+	public Client hostIgnoringClient () throws Exception {
+		
+		TrustManager[] trustManagers = new TrustManager[] {
+				new X509TrustManager() {
+					
+					@Override
+					public X509Certificate[] getAcceptedIssuers() {
+						
+						return null;
+					}
+					
+					@Override
+					public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+						
+						
+					}
+					
+					@Override
+					public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+						
+						
+					}
+				}
+		};
+		
+		SSLContext sslContext = SSLContext.getInstance("TLS");
+		sslContext.init(null, trustManagers, new SecureRandom());
+		
+		Set<String> hostNameList = new HashSet<String>();
+		hostNameList.add(this.hostname);
+		HttpsURLConnection.setDefaultHostnameVerifier(new IgnoreHostNameSSL(hostNameList));
+		
+		HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+		
+		DefaultClientConfig config = new DefaultClientConfig();
+		Map<String, Object> properties = config.getProperties();
+		HTTPSProperties httpsProperties = new HTTPSProperties(new HostnameVerifier() {
+			
+			@Override
+			public boolean verify(String hostname, SSLSession session) {
+				return true;
+			}
+		}, sslContext);
+		
+		properties.put(httpsProperties.PROPERTY_HTTPS_PROPERTIES, httpsProperties);
+		config.getClasses().add(JacksonJsonProvider.class);
+		config.getClasses().add(MultiPartWriter.class);
+		
+		return Client.create(config);
+		
+	}
+}
